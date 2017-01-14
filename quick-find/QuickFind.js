9cf2a1d7cdf5504d69a410d880b6c284cb18740e
Bruno Giovanini <bsgiovanini@gmail.com>
2017-01-14 12:08:51 -0200

    quick-find

--- quick-find/QuickFind.js
+++ quick-find/QuickFind.js
@@ -3,10 +3,30 @@ export default class QuickFind {
     constructor(n) {
         this.id = [];
         for(let i = 1; i <= n; i++ ) {
-            this.id.push(i);
+            this.id[i] = i;
         }
+        this.counter = n;
     }
     
-    find (p) { this.id[p] }
+    find (p) { return this.id[p] }
+
+    union(p,q) {
+    	
+    	let pID = this.find(p);
+		let qID = this.find(q);
+
+		if (pID !== qID) {
+			for (let i = 1; i < this.id.length; i++)
+				if (this.id[i] == pID) this.id[i] = qID;
+			this.counter--;
+		}
+
+		return this;
+    }
+
+    isConnected(p, q) {
+    	return this.find(p) === this.find(q);
+    }
+
 
 }
--- quick-find/__tests__/quick-find-tests.js
+++ quick-find/__tests__/quick-find-tests.js
@@ -1,4 +1,3 @@
-
 expect.extend({
   arraysToBeEqual(actual, received) {
     const pass = received.length === actual.length && received.every((r,i) => r === actual[i]);
@@ -8,12 +7,38 @@ expect.extend({
   }
 });
 
+let QuickFind = null;
+
+beforeAll(function() {
+	QuickFind = require('../QuickFind').default;
+})
+
 test('should construct vector [1,2]', () => {
-    let QuickFind = require('../QuickFind').default;
-    console.log(QuickFind);
-    expect(new QuickFind(2).id).arraysToBeEqual([1, 2]);
+    
+    expect(new QuickFind(2).id).arraysToBeEqual([undefined, 1, 2]);
+});
+
+test('should return 3 when finding 3 in a recently created array with size 5', () => {
+	const qf = new QuickFind(5);
+	expect(qf.find(3)).toBe(3);
+	expect(qf.counter).toBe(5);
 });
 
 
+test('should return 1 after union (3,1) when finding 3', () => {
+	const qf = new QuickFind(5);
+	expect(qf.union(3,1).find(3)).toBe(1); 
+	expect(qf.counter).toBe(4);
+	expect(qf.isConnected(3,1)).toBeTruthy();
+	expect(qf.isConnected(1,3)).toBeTruthy();
 
+})
+
+test('should return 1 after unions (3,1) and (5,3) when finding 5', () => {
+	const qf = new QuickFind(5);
+	expect(qf.union(3,1).union(5,3).find(5)).toBe(1); 
+	expect(qf.counter).toBe(3);
+	expect(qf.isConnected(3,5)).toBeTruthy();
+	expect(qf.isConnected(1,5)).toBeTruthy();
+})
 
