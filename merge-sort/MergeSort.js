ecbdf776cdbc0bab4a9d03acdad138557fd0b926
Bruno Giovanini <bsgiovanini@gmail.com>
2017-02-08 10:27:27 +0000

    mergesort

---
+++ merge-sort/MergeSort.js
@@ -0,0 +1,56 @@
+export default class MergeSort {
+    
+    constructor(array, comparator = null) {
+        if(!Array.isArray(array)) {
+            throw new Error("invalid array");
+        }
+        
+        if (comparator !== null && typeof(comparator) !== 'function') {
+            throw new Error("invalid comparator");
+        }
+        if (comparator !== null && typeof(comparator) === 'function' && comparator.length !== 2) {
+            throw new Error("invalid comparator");
+        }
+        
+        this.array = array;
+        this.comparator = comparator != null? comparator: this.comparatorDefault;
+        
+    }
+    
+    
+    
+    comparatorDefault(item1, item2) {
+        return (item1 < item2)? -1: (item1 > item2)? 1 : 0;
+    }
+    
+    merge(array, low, mid, hi) {
+       
+       let aux = [...array];
+       
+       let i = low, j = mid+1;
+       
+       for(let k = low; k <= hi; k++) {
+           if (i > mid) array[k] = aux[j++];
+           else if (j > hi) array[k] = aux[i++];
+           else if (this.comparator(aux[j],aux[i]) < 0) array[k] = aux[j++];
+           else array[k] = aux[i++];
+       } 
+
+    }
+
+    sort() {
+        this._sort(this.array, 0, this.array.length-1);
+        return this.array;
+    }
+    
+    _sort(array, lo, hi) {
+        if (hi <= lo) return;
+        let mid = Math.floor(lo + (hi - lo)/2);
+        this._sort(array, lo, mid);
+        this._sort(array, mid+1, hi);
+        this.merge(array, lo, mid, hi);
+        
+    }
+    
+    
+}
---
+++ merge-sort/__tests__/merge-sort-tests.js
@@ -0,0 +1,52 @@
+expect.extend({
+  arraysToBeEqual(actual, received) {
+    const pass = received.length === actual.length && received.every((r,i) => r === actual[i]);
+    const message =
+      () => `expected ${received} ${pass ? 'not ' : ''} to be ${actual}`;
+    return {message, pass};
+  }
+});
+
+let MergeSort = null;
+
+beforeAll(function() {
+	MergeSort = require('../MergeSort').default;
+})
+
+test('should throw error if array argument is invalid', () => {
+
+    expect(function() { const bs = new MergeSort(2); }).toThrowError(/invalid array/);
+    expect(function() { const bs = new MergeSort(); }).toThrowError(/invalid array/);
+    expect(function() { const bs = new MergeSort(null); }).toThrowError(/invalid array/);
+    expect(function() { const bs = new MergeSort(undefined); }).toThrowError(/invalid array/);
+
+});
+
+test('should merge two arrays in asc order', () => {
+    expect(function() {
+      let array = [2,3]; 
+      new MergeSort([]).merge(array, 0, 0, 1);
+      return array;
+    }()).arraysToBeEqual([2,3]);
+    expect(function() {
+      let array = [2,7,5,8]; 
+      new MergeSort([]).merge(array,0, 1, 3);
+      return array;
+    }()).arraysToBeEqual([2,5,7,8]);
+});
+
+test('should sort an array', () => {
+    expect(function() {
+      const m = new MergeSort([2,3]);
+      return m.sort();
+    }()).arraysToBeEqual([2,3]);
+    expect(function() {
+      const m = new MergeSort([2,7,5,8]);
+      return m.sort();
+    }()).arraysToBeEqual([2,5,7,8]);
+});
+
+
+
+
+
